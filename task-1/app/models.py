bf69e7900a343a91947609ed0687daefe5eb3534
Sahitha02 <sahithacheekatla02@gmail.com>
2025-11-29 23:01:30 +0530

    Task-2 optmized

--- task-1/app/models.py
+++ task-1/app/models.py
@@ -2,12 +2,15 @@ from .db import db
 
 class User(db.Model):
     __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    posts = db.relationship("Post", backref="author")  # lazy-loaded by default
+
+    posts = db.relationship("Post", backref="author")   # lazy-loaded (N+1)
 
 class Post(db.Model):
     __tablename__ = "posts"
+
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     title = db.Column(db.String(100))
